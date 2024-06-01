d89523b5618df83495f4c6b2f65af981502e6dfd
Heba Elzaher <Hebaelzaherr@gmail.com>
2024-06-01 21:43:19 +0400

    done

---
+++ src/main/java/com/example/assigmentthree/controller/ApplicationController.java
@@ -0,0 +1,61 @@
+package com.example.assigmentthree.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/")
+    public String getPage() {
+        return "HomePage";
+    }
+
+    @GetMapping(value = "/signUp")
+    public String signUp() {
+        return "signUp";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = "/twoFactorAuth")
+    public String twoFactorAuth(@RequestParam(name = "email") String email, Model model) {
+        model.addAttribute("email", email);
+        return "twoFactorAuth";
+    }
+
+    @GetMapping(value = "/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping(value = "/uploadPage")
+    public String uploadPhoto() {
+        return "uploadPage";
+    }
+
+    @GetMapping(value = "/history")
+    public String history() {
+        return "history";
+    }
+
+    @GetMapping(value = "/points")
+    public String points() {
+        return "points";
+    }
+
+    @GetMapping(value = "/treeInfoPage")
+    public String treeInfo() {
+        return "treeInfo";
+    }
+
+    @GetMapping(value = "/updateUser")
+    public String updateUser() {
+        return "updateUser";
+    }
+}
